f0757cd2b2e8fe18f14ef75c8c184a5d22ff76fc
takahiro.tsukada <aqsh.reprom@gmail.com>
2019-12-07 10:35:24 +0900

    notificationのviewsからoffer_listへのオブジェクトの渡し方を変更
    - views側でフィルタリング(queryset)

--- notification/views.py
+++ notification/views.py
@@ -9,25 +9,21 @@ class OfferListView(ListView):
     context_object_name = 'offer_list'
     template_name = 'offer_list.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+    def get_queryset(self):
         user = self.request.user
         target_object = JobSeeker.objects.get(user_id=user.id)
         my_job_seeker_id = target_object.id
-        context['my_job_seeker_id'] = my_job_seeker_id
-        return context
+        return OfferList.objects.filter(job_seeker_id=my_job_seeker_id)
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     user = self.request.user
+    #     target_object = JobSeeker.objects.get(user_id=user.id)
+    #     my_job_seeker_id = target_object.id
+    #     context['my_job_seeker_id'] = my_job_seeker_id
+    #     return context
 
 
 class OfferDetailView(generic.DetailView):
     model = OfferList
     template_name = 'offer_list_detail.html'
-
-# class JobSeekerNoticeListView(ListView):
-#     model = JobSeekerNotice
-#     context_object_name = 'jobseekers'
-#     template_name = 'offer_list.html'
-#
-#
-# class JobSeekerNoticeDetailView(generic.DetailView):
-#     model = JobSeekerNotice
-#     template_name = 'job_seekers_detail.html'
