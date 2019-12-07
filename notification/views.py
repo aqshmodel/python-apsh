02a2dc16ee00417b7fdb10b2af9de5f1b3dafa9f
takahiro.tsukada <aqsh.reprom@gmail.com>
2019-12-07 10:17:39 +0900

    notification,searchの挙動修正
    - 求職者一覧で自分自身を表示しないようにする
    - 自分へきたオファーのみを表示する挙動完成

--- notification/views.py
+++ notification/views.py
@@ -1,13 +1,21 @@
 from django.views import generic
-from django.views.generic import ListView, DetailView
-
+from django.views.generic import ListView
 from offer.models import OfferList
+from registration.models import JobSeeker
 
 
 class OfferListView(ListView):
     model = OfferList
     context_object_name = 'offer_list'
     template_name = 'offer_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+        target_object = JobSeeker.objects.get(user_id=user.id)
+        my_job_seeker_id = target_object.id
+        context['my_job_seeker_id'] = my_job_seeker_id
+        return context
 
 
 class OfferDetailView(generic.DetailView):
--- search/views.py
+++ search/views.py
@@ -1,13 +1,19 @@
 from django.views import generic
 from django.views.generic import ListView
 
-from registration.models import User
+from registration.models import User, JobSeeker
 
 
 class UserListView(ListView):
     model = User
     context_object_name = 'jobseekers'
     template_name = 'job_seekers_list.html'
+
+    def get_queryset(self):
+        user = self.request.user
+        target_object = JobSeeker.objects.get(user_id=user.id)
+        my_job_seeker_id = target_object.id
+        return User.objects.exclude(jobseeker__id=None).exclude(jobseeker__id=my_job_seeker_id)
 
 
 class UserDetailView(generic.DetailView):
